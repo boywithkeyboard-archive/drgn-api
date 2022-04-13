aa5ed516d414a229f68b56a0ab9f19c823fd748b
Azury <azury.wtf@gmail.com>
2022-04-13 12:46:43 +0200

    added watch script

---
+++ scripts/watch.mjs
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import { build } from 'esbuild'
+
+(async () => {
+  await build({
+    entryPoints: ['./src/index.ts'],
+    outfile: './build/index.js',
+    bundle: true,
+    minify: true,
+    watch: true,
+    platform: 'node',
+    define: {
+      DEV: true,
+      ENCRYPTION_KEY: JSON.stringify(process.env.ENCRYPTION_KEY),
+      JWT_SECRET: JSON.stringify(process.env.JWT_SECRET),
+      MONGO: JSON.stringify(process.env.MONGO),
+      APP: JSON.stringify(process.env.APP),
+      API: JSON.stringify(process.env.API),
+      PORT: JSON.stringify(process.env.PORT),
+      GITHUB_CLIENT: JSON.stringify(process.env.GITHUB_CLIENT),
+      GITHUB_SECRET: JSON.stringify(process.env.GITHUB_SECRET),
+      RATE_LIMIT: Infinity
+    }
+  })
+})()
