ea2b02e3f85c0f7423326e9c7ed71e233288f8b8
Azury <azury.wtf@gmail.com>
2022-04-13 03:40:39 +0200

    edited team schema

--- src/schemas/team.ts
+++ src/schemas/team.ts
@@ -4,13 +4,15 @@ export type TeamDocument = {
   _id: string,
   name: string,
   admin: string,
+  members: string[],
   createdAt: string,
   updatedAt: string
 }
 
 const teamSchema: Schema = new mongoose.Schema({
   name: String,
-  admin: String
+  admin: String,
+  members: [String]
 }, { timestamps: true })
 
 export default mongoose.model<TeamDocument>('teams', teamSchema)
