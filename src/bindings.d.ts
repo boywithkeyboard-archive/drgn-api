0aa5d0e5419b5efa772cf3f36b0f56ca123acc24
Azury <azury.wtf@gmail.com>
2022-04-13 03:36:57 +0200

    added bindings

---
+++ src/bindings.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  const ENCRYPTION_KEY: string
+  const JWT_SECRET: string
+  const MONGO: string
+  const DEV: boolean
+  const APP: string
+  const API: string
+  const PORT: string
+  const GITHUB_CLIENT: string
+  const GITHUB_SECRET: string
+  const RATE_LIMIT: number
+}
+
+export {}
