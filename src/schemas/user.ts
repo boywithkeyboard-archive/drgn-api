d55b704891bcb558e609a33c7e94db653d04b9f3
Azury <azury.wtf@gmail.com>
2022-06-06 22:33:15 +0200

    added `user` schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,16 +1,19 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose from 'mongoose'
+import type { Schema } from 'mongoose'
 
 export type UserDocument = {
   _id: string,
-  github: string,
-  teams: string[],
+  email: string,
+  password?: string,
+  secret?: string,
   createdAt: string,
   updatedAt: string
 }
 
 const userSchema: Schema = new mongoose.Schema({
-  github: String,
-  teams: [String]
+  email: String,
+  password: String,
+  secret: String
 }, { timestamps: true })
 
 export default mongoose.model<UserDocument>('users', userSchema)
