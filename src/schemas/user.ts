e2cd5e7aabb1214129e3d00e7fe870f475b64833
Azury <azury.wtf@gmail.com>
2022-06-11 16:42:15 +0200

    edited user schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -2,18 +2,22 @@ import mongoose from 'mongoose'
 import type { Schema } from 'mongoose'
 
 export type UserDocument = {
-  _id: string,
-  email: string,
-  password?: string,
-  secret?: string,
-  createdAt: string,
+  _id: string
+  email: string
+  password?: string
+  secret?: string // for 2fa
+  passwordless?: boolean
+  twoFactor?: boolean
+  createdAt: string
   updatedAt: string
 }
 
 const userSchema: Schema = new mongoose.Schema({
   email: String,
   password: String,
-  secret: String
+  secret: String,
+  passwordless: Boolean,
+  twoFactor: Boolean
 }, { timestamps: true })
 
 export default mongoose.model<UserDocument>('users', userSchema)
