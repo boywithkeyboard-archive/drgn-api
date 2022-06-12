cd08cac4403864daee7d386658385f7aadab1ab2
Azury <azury.wtf@gmail.com>
2022-06-12 12:13:40 +0200

    fixed `validateSession` module

--- src/modules/validateSession.ts
+++ src/modules/validateSession.ts
@@ -1,28 +1,10 @@
-import { FastifyRequest } from 'fastify'
-import ThrowableError from './throwableError'
-import userSchema from '../schemas/user'
-import { userCache } from './cache'
-
-const currentUser = async (_id: string) => {
-  if (await userCache.get(_id)) return (await userCache.get(_id))?.value
-
-  const user = await userSchema.findById(_id)
-  await userCache.set(_id, user)
-
-  return user
-}
+import type { FastifyRequest } from 'fastify'
 
 const validateSession = async (request: FastifyRequest) => {
   try {
-    const payload = await request.jwtVerify() as {
-      user: string,
-      bot: boolean
-    }
-
-    request['currentUser'] = await currentUser(payload.user)
-    request['bot'] = payload.bot
+    await request.jwtVerify()
   } catch (err) {
-    throw new ThrowableError('Invalid Session')
+    throw new Error('invalid session')
   }
 }
 
