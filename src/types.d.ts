a9b82fc38d6d946fef43a5affef106f89c1ce848
Azury <azury.wtf@gmail.com>
2022-06-12 12:11:34 +0200

    added type declarations for `@fastify/jwt`

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,12 @@
+import type { UserDocument } from './schemas/user'
 import type { FastifyInstance } from 'fastify'
+import '@fastify/jwt'
+
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    payload: { [key: string]: any }
+    user: UserDocument
+  }
+}
 
 export type Router = FastifyInstance
