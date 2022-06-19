0c41b0a0db6a5093ae57387a0c1edb4801da83f1
Azury <azury.wtf@gmail.com>
2022-06-19 15:42:10 +0200

    edited server schema

--- src/schemas/servers.ts
+++ src/schemas/servers.ts
@@ -3,21 +3,27 @@ import type { Schema } from 'mongoose'
 
 export type ServerDocument = {
   _id: string
-  host: string
-  port: number
-  username: string
-  password?: string
-  privateKey?: string
+  user: string
+  credentials: {
+    host: string
+    port: number
+    username: string
+    password?: string
+    privateKey?: string
+  }
   createdAt: string
   updatedAt: string
 }
 
 const schema: Schema = new mongoose.Schema({
-  host: String,
-  port: Number,
-  username: String,
-  password: String,
-  privateKey: String
+  user: String,
+  credentials: {
+    host: String,
+    port: Number,
+    username: String,
+    password: String,
+    privateKey: String
+  }
 }, { timestamps: true })
 
 const serverSchema = mongoose.model<ServerDocument>('servers', schema)
